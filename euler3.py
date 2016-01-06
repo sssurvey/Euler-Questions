385dee2ef674941aa7c973235669e00ccc104bc6
Your Name <you@example.com>
2016-01-05 21:56:59 -0600

    do it tomorrow

---
+++ euler3.py
@@ -0,0 +1,10 @@
+#python programing questions No.3
+#Largest prime facter
+#The prime factor of 13195 are 5, 7, 13 and 29
+#what is the largest prime factor of the number 600851475143
+
+#version 0.1
+num0 =13195
+primeNums =[2,3]
+primeTemp =0
+
