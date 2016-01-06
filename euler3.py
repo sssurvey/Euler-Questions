503255921959e04354f5596ecddaecacd0e4b5bf
Your Name <you@example.com>
2016-01-06 10:20:29 -0600

    question finished

--- euler3.py
+++ euler3.py
@@ -5,24 +5,42 @@
 
 #version 0.1
 import random
+from fractions import gcd
 
 #cited Pollard Rho
-def function(number):
+#https://comeoncodeon.wordpress.com
+'''def pollardRho(number):
 	if number %2 ==0:
 		return 2
 	n0 =random.randint(1, number -1)
 	n1 =n0
 	n2 =random.randint(1, number -1)
 	n3 = 1
-	while g ==1:
+	while n3 ==1:
 		n0 =((n0 *n0) %number +n2) %number
 		n1 =((n1 *n1) %number +n2) %number
 		n1 =((n1 *n1) %number +n2) %number
 		n3 =gcd(abs(n0 -n1), number)
-	return n3
+	return n3'''
 
-num0 =13195
-primeNums =[2,3]
-primeTemp =0`
+
+#cited stackoverflow.com/questions/15347174
+def prime_factors(n):
+	i =2
+	factors =[]
+	while i *i <=n:
+		if n %i:
+			i +=1
+		else:
+			n //=1
+			factors.append(i)
+	if n >1:
+		factors.append(n)
+	return factors
+
+print(prime_factors())
+
+'''-------------------------------------------------------------------------'''
+
 
 
