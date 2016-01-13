df46a0ca775726809ff89d11da3029dda19fb604
Your Name <haominshi@gmail.com>
2016-01-12 18:46:54 -0600

    not finished

--- euler00012.py
+++ euler00012.py
@@ -16,8 +16,65 @@
 
 #version 0.1
 
-def findTriangular():
-	t0 =0
-	t1 =0
-	
+def makeTrian():
+    number =4
+    trian =0
+    divider =1
+    temp0 =0
+    listOfD =[]
+    sum0 =0
+
+    for i in range(0,5):
+        trian = number*(number+ 1)/2
+
+        while True:
+            temp0 =trian %divider #test if trian can be divided in to int
+            print (temp0)
+
+            if temp0 ==0:
+                listOfD.append(divider)
+                print('append')
+            if temp0 !=0:
+                print('do nothing to list, increase divider')
+
+            divider =divider +1 #increase divider
+            print(listOfD)
+
+
+            if divider == trian and len(listOfD) < 5:
+                listOfD.append(divider)
+                print('Trian need increase--')
+                break
+
+            if len(listOfD)==5:
+                sys.exit('x')
+
+
+
+
+
+
+                '''if len(listOfD) ==5:
+                    print('Done!')
+                    for i in range(0, len(listOfD)):
+                        sum0 =listOfD[i]+sum0
+                        print(sum0)
+                    print('finished')
+                    print(listOfD)
+                    print("number" +str(number))
+                    sys.exit("done")
+
+                '''
+
+
+        print('test')
+        listOfD =[]
+        number =number +1
+
+'''----------------------------------------------------------'''
+
+
+
+print (makeTrian())
+
 
