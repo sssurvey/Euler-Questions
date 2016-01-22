59125815f8397f782e61fa401ee59f49cfac8e1f
Your Name <haominshi@gmail.com>
2016-01-21 23:51:37 -0600

    update

--- euler00015.py
+++ euler00015.py
@@ -1,1 +1,17 @@
 #python problem 15
+
+#grid = [[0 for i in range(20)] for i in range(20)]
+
+grid =[]
+
+for row in range(20):
+	grid.append([])
+	for column in range(20):
+		grid[row].append('|x|')
+
+def printGrid(grid):
+	for row in grid:
+		print ' '.join(row)
+
+'''-----------------------------------------------------------'''
+printGrid(grid)
